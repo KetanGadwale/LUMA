89610376ea60c7beee68e729adfdaae836522ab8
KetanGadwale <ketanvgadwale@gmail.com>
2022-08-02 18:39:23 +0530

    lamba test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+    const [state, setState] = useState('');
+
+    useEffect(() => {
+        async function getDataFromLambda() {
+            const res = await fetch(
+                'https://4736oofnuj34o7fpeskgmlxk5u0ekatp.lambda-url.us-east-1.on.aws/'
+            );
+            const data = await res.json();
+            console.log(data);
+            setState(data);
+        }
+        getDataFromLambda();
+    }, []);
+
     return (
         <div className='App'>
-            <h1>LUMA : Powering your financial Dream</h1>
+            <h1>LUMA : Powering your financial Dream. {state} </h1>
         </div>
     );
 }
